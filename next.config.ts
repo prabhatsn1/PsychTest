941a04e3516678af841622e483788e4adc5d394d
Prabhat Soni <52910557+prabhatsn1@users.noreply.github.com>
2026-05-30 22:57:36 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: ["canvas", "tesseract.js", "pdfjs-dist"],
 };
 
 export default nextConfig;
